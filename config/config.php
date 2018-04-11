7aeececd665439c86c71336edcebe86709f3f7d0
Nathan <me@nwilce.co.uk>
2018-04-11 20:52:59 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,27 +1,22 @@
 <?php
-class Config
-{
-    public static function initstatic()
-    {
-        $db_config = array
-        (
-            "DB_HOST" => "localhost",
-            "DB_USER" => "",
-            "DB_PASS" => "",
-            "DB_NAME" => "",
-            "DB_PREFIX" => ""
-        );
-        $config = array
-        (
-            "URL" => "",
-            
-            "name" => "CoreFramework",
-            "description" => "CoreFramework is a simple MVC framework built for speed",
-            "author" => "Nathan Wilce",
-            
-            "template_cache_dir" => "helpers/template_cache"
-        );
-        Registry::set("db_config", $db_config);
-        Registry::set("config", $config);
-    }
-}
+
+$db_config = array
+(
+    "DB_HOST" => "localhost",
+    "DB_USER" => "",
+    "DB_PASS" => "",
+    "DB_NAME" => "",
+    "DB_PREFIX" => ""
+);
+$config = array
+(
+    "URL" => "",
+
+    "name" => "CoreFramework",
+    "description" => "CoreFramework is a simple MVC framework built for speed",
+    "author" => "Nathan Wilce",
+
+    "template_cache_dir" => "helpers/template_cache"
+);
+Registry::set("db_config", $db_config);
+Registry::set("config", $config);
