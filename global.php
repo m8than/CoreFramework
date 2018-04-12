04f4543f9c7c78d2370d5ed61d94e9d008edb64b
Nathan <me@nwilce.co.uk>
2018-04-12 02:26:57 +0100

    Update global.php

--- global.php
+++ global.php
@@ -22,4 +22,12 @@ require_once ROOT . H . 'general.php';
 require_once ROOT . H . 'template.php';
 
 require_once ROOT . 'router.php';
+
+
+$db_config = Registry::get('db_config');
+set DB object
+Registry::set('db', new Database($db_config['DB_HOST'], $db_config['DB_NAME'],
+                                $db_config['DB_USER'], $db_config['DB_PASS'],
+                                $db_config['DB_PREFIX']));
+                                */
 ?>
