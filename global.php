0e74efc3106f667b092b71cf7478f2cb56a7dfc8
Nathan Wilce <me@nwilce.co.uk>
2018-04-11 19:52:14 -0700

    update

--- global.php
+++ global.php
@@ -11,6 +11,8 @@ DEFINE('H','helpers/');
 session_start();
 
 require_once ROOT . 'registry.php';
+require_once ROOT . 'router.php';
+
 require_once ROOT . APP . 'controller.php';
 require_once ROOT . APP . 'model.php';
 
@@ -20,8 +22,6 @@ require_once ROOT . C . 'routes.php';
 require_once ROOT . H . 'database.php';
 require_once ROOT . H . 'general.php';
 require_once ROOT . H . 'template.php';
-
-require_once ROOT . 'router.php';
 
 $db_config = Registry::get('db_config');
 Registry::set('db', new Database($db_config['DB_HOST'], $db_config['DB_NAME'],
